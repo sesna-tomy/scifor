4f49e0254c219ac1887df77602abe82de01fb9cd
sesna-tomy <147467610+sesna-tomy@users.noreply.github.com>
2023-10-29 14:13:33 +0530

    Update Merge_strings_alternatelty.py

--- Assignment_3/Basic Implementation/Merge_strings_alternatelty.py
+++ Assignment_3/Basic Implementation/Merge_strings_alternatelty.py
@@ -1,1 +1,22 @@
+'''
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, 
+starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+'''
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+       z=""
+       i=0
+       while i < len(word1) and i < len(word2):
+           z+=word1[i]
+           z+=word2[i]
+           i+=1
+       while i < len(word1):
+           z+=word1[i]
+           i+=1
+       while i < len(word2):
+           z+=word2[i]
+           i+=1
+       return z 
 
+sol = Solution()
+sol.mergeAlternately("abc","pqr") 
