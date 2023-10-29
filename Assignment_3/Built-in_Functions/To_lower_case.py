23062a6be506b57c0a6422fb55b4de668f06a538
sesna-tomy <147467610+sesna-tomy@users.noreply.github.com>
2023-10-29 15:21:49 +0530

    Create To_lower_case.py

---
+++ Assignment_3/Built-in_Functions/To_lower_case.py
@@ -0,0 +1,8 @@
+# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter
+class Solution(object):
+    def LowerCase(self, s):
+       lower = s.lower()
+       return lower
+
+sol = Solution()
+sol.LowerCase("Hello")
