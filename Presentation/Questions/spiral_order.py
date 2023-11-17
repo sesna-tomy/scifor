3ba8f1223e7585482f9c6980a5ddd66793be8b8f
sesna-tomy <147467610+sesna-tomy@users.noreply.github.com>
2023-11-17 21:17:30 +0530

    Add files via upload

---
+++ Presentation/Questions/spiral_order.py
@@ -0,0 +1,23 @@
+'''
+Given an m x n matrix, return all elements of the matrix in spiral order.
+'''
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        result = []
+        while matrix:
+            result += matrix.pop(0)
+            if matrix and matrix[0]:
+                for row in matrix:
+                    result.append(row.pop())
+            if matrix:
+                result += matrix.pop()[::-1]
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    result.append(row.pop(0))
+        return result
+    
+
+sol =Solution()
+spiral = sol.spiralOrder([[1,2,3],[4,5,6],[7,8,9]])
+print(spiral)
